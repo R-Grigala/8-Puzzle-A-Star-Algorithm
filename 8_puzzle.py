d684256bb59b2c5222d3c3309ed72e989d507085
R-Grigala <r.grigalashvili777@gmail.com>
2023-06-17 17:43:12 +0400

    Coding Continue:// Implementation Algorithm

--- 8_puzzle.py
+++ 8_puzzle.py
@@ -1,30 +1,29 @@
 class Node:
-    def __init__(self,data,level,fval):
-        """ Initialize the node with the data, level of the node and the calculated fvalue """
+    def __init__(self, data, level, fval):
+        """ Initialize the node with the data, level of the node, and the calculated fvalue """
         self.data = data
         self.level = level
         self.fval = fval
 
     def generate_child(self):
         """ Generate child nodes from the given node by moving the blank space
-            either in the four directions {up,down,left,right} """
-        x,y = self.find(self.data,'_')
+            either in the four directions {up, down, left, right} """
+        x, y = self.find(self.data, '_')
         """ val_list contains position values for moving the blank space in either of
-            the 4 directions [up,down,left,right] respectively. """
-        val_list = [[x,y-1],[x,y+1],[x-1,y],[x+1,y]]
+            the 4 directions [up, down, left, right] respectively. """
+        val_list = [[x, y - 1], [x, y + 1], [x - 1, y], [x + 1, y]]
         children = []
         for i in val_list:
-            child = self.shuffle(self.data,x,y,i[0],i[1])
+            child = self.shuffle(self.data, x, y, i[0], i[1])
             if child is not None:
-                child_node = Node(child,self.level+1,0)
+                child_node = Node(child, self.level + 1, 0)
                 children.append(child_node)
         return children
-        
-    def shuffle(self,puz,x1,y1,x2,y2):
-        """ Move the blank space in the given direction and if the position value are out
-            of limits the return None """
+
+    def shuffle(self, puz, x1, y1, x2, y2):
+        """ Move the blank space in the given direction and if the position values are out
+            of limits, then return None """
         if x2 >= 0 and x2 < len(self.data) and y2 >= 0 and y2 < len(self.data):
-            temp_puz = []
             temp_puz = self.copy(puz)
             temp = temp_puz[x2][y2]
             temp_puz[x2][y2] = temp_puz[x1][y1]
@@ -32,65 +31,63 @@ class Node:
             return temp_puz
         else:
             return None
-            
 
-    def copy(self,root):
-        """ Copy function to create a similar matrix of the given node"""
+    def copy(self, root):
+        """ Copy function to create a similar matrix of the given node """
         temp = []
         for i in root:
             t = []
             for j in i:
                 t.append(j)
             temp.append(t)
-        return temp    
-            
-    def find(self,puz,x):
+        return temp
+
+    def find(self, puz, x):
         """ Specifically used to find the position of the blank space """
-        for i in range(0,len(self.data)):
-            for j in range(0,len(self.data)):
+        for i in range(0, len(self.data)):
+            for j in range(0, len(self.data)):
                 if puz[i][j] == x:
-                    return i,j
+                    return i, j
 
 
 class Puzzle:
-    def __init__(self,size):
-        """ Initialize the puzzle size by the specified size,open and closed lists to empty """
+    def __init__(self, size):
+        """ Initialize the puzzle size by the specified size, open and closed lists to empty """
         self.n = size
         self.open = []
         self.closed = []
 
     def accept(self):
-        """ Accepts the puzzle from the user """
+        """ Accept the puzzle from the user """
         puz = []
-        for i in range(0,self.n):
+        for i in range(0, self.n):
             temp = input().split(" ")
             puz.append(temp)
         return puz
 
-    def f(self,start,goal):
-        """ Heuristic Function to calculate hueristic value f(x) = h(x) + g(x) """
-        return self.h(start.data,goal)+start.level
+    def f(self, start, goal):
+        """ Heuristic Function to calculate heuristic value f(x) = h(x) + g(x) """
+        return self.h(start.data, goal) + start.level
 
-    def h(self,start,goal):
-        """ Calculates the different between the given puzzles """
+    def h(self, start, goal):
+        """ Calculate the difference between the given puzzles """
         temp = 0
-        for i in range(0,self.n):
-            for j in range(0,self.n):
+        for i in range(0, self.n):
+            for j in range(0, self.n):
                 if start[i][j] != goal[i][j] and start[i][j] != '_':
                     temp += 1
         return temp
-        
 
     def process(self):
-        """ Accept Start and Goal Puzzle state"""
-        print("Enter the start state matrix \n")
+        """ Accept Start and Goal Puzzle state """
+        print("Enter the start state matrix:")
         start = self.accept()
-        print("Enter the goal state matrix \n")        
+        print("Enter the goal state matrix:")
         goal = self.accept()
 
-        start = Node(start,0,0)
-        start.fval = self.f(start,goal)
-        """ Put the start node in the open list"""
+        start = Node(start, 0, 0)
+        start.fval = self.f(start, goal)
+        """ Put the start node in the open list """
         self.open.append(start)
         print("\n\n")
         while True:
@@ -101,23 +98,44 @@ class Puzzle:
             print(" \\\'/ \n")
             for i in cur.data:
                 for j in i:
-                    print(j,end=" ")
+                    print(j, end=" ")
                 print("")
-            """ If the difference between current and goal node is 0 we have reached the goal node"""
-            if(self.h(cur.data,goal) == 0):
+            """ If the difference between the current and goal node is 0, we have reached the goal node """
+            if self.h(cur.data, goal) == 0:
                 break
             for i in cur.generate_child():
-                i.fval = self.f(i,goal)
+                i.fval = self.f(i, goal)
                 self.open.append(i)
             self.closed.append(cur)
             del self.open[0]
 
-            """ sort the opne list based on f value """
-            self.open.sort(key = lambda x:x.fval,reverse=False)
+            """ sort the open list based on f value """
+            self.open.sort(key=lambda x: x.fval, reverse=False)
 
 
 puz = Puzzle(3)
 puz.process()
 
 
+"""
 
+Time Complexity Analysis:
+
+The time complexity of the A* algorithm depends on the size of the puzzle (n) and the number of nodes expanded.
+In the worst case, the algorithm explores all possible states until it finds the goal state.
+Assuming there are k possible states reachable from each node, the worst-case time complexity can be approximated as O(k^d), 
+where d is the depth of the goal state from the start state.
+In this case, k is at most 4 (four possible directions to move the empty space), and d is unknown beforehand.
+
+Space Complexity Analysis:
+
+The space complexity of the A* algorithm depends on the maximum number of nodes stored in the open and closed lists during the search.
+In the worst case, where all nodes need to be explored, the maximum number of nodes in the open list is proportional to the number of expanded nodes.
+Therefore, the worst-case space complexity can be approximated as O(k^d), similar to the time complexity.
+
+Note: The actual time and space complexity may vary depending on the specific puzzle instance and the efficiency of the heuristic function.
+
+Overall, the code is well-structured and implements the A* algorithm for solving puzzles.
+It would be helpful to have additional information about the specific puzzle being solved to provide more accurate time and space complexity analysis.
+
+"""
--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # 8-Puzzle-A-Star-Algorithm
 8 Puzzle Python Implementation Using A* Algorithm
 
+# A* Algorithm
+
+A* is a computer algorithm that is widely used in pathfinding and graph traversal, 
+the process of plotting an efficiently traversable path between multiple points, called nodes. 
+Noted for its performance and accuracy, it enjoys widespread use.
+The key feature of the A* algorithm is that it keeps a track of each visited node 
+which helps in ignoring the nodes that are already visited, saving a huge amount of time. 
+It also has a list that holds all the nodes that are left to be explored and it chooses the most optimal node from this list, 
+thus saving time not exploring unnecessary or less optimal nodes.
+
 # Comments and Analysis
 
 1. The code implements the A* algorithm to solve a puzzle. The puzzle is represented as an n x n matrix, where each element can be a number or the underscore character _ representing the empty space.
@@ -15,3 +25,24 @@
 10. The h method of the Puzzle class calculates the number of misplaced tiles (excluding the empty space) between two puzzle states.
 11. The process method of the Puzzle class performs the A* algorithm. It initializes the start node, calculates its f-value, and adds it to the open list. Then, it enters a loop where it selects the node with the lowest f-value from the open list, generates its child nodes, calculates their f-values, and adds them to the open list. The current node is moved to the closed list. The loop continues until the goal node is reached (the difference between the current and goal nodes is 0).
 12. The open list is sorted based on f-value in each iteration, ensuring that the node with the lowest f-value is selected next
+
+
+# Time Complexity Analysis:
+
+The time complexity of the A* algorithm depends on the size of the puzzle (n) and the number of nodes expanded.
+In the worst case, the algorithm explores all possible states until it finds the goal state.
+Assuming there are k possible states reachable from each node, the worst-case time complexity can be approximated as O(k^d), 
+where d is the depth of the goal state from the start state.
+In this case, k is at most 4 (four possible directions to move the empty space), and d is unknown beforehand.
+
+# Space Complexity Analysis:
+
+The space complexity of the A* algorithm depends on the maximum number of nodes stored in the open and closed lists during the search.
+In the worst case, where all nodes need to be explored, the maximum number of nodes in the open list is proportional to the number of expanded nodes.
+Therefore, the worst-case space complexity can be approximated as O(k^d), similar to the time complexity.
+
+Note: The actual time and space complexity may vary depending on the specific puzzle instance and the efficiency of the heuristic function.
+
+Overall, the code is well-structured and implements the A* algorithm for solving puzzles.
+It would be helpful to have additional information about the specific puzzle being solved to provide more accurate time and space complexity analysis.
+![Alt text](8-puzzle-problem-in-table.jpg)
